4a5d1ffa13a7e5b92048e843cd7b1e803a5201cb
Rohit Chaudhary <65174148+rohitchaudhary-mnnit@users.noreply.github.com>
2021-09-22 18:01:02 +0545

    updating the App.js

--- HIE-FrontEnd/src/App.js
+++ HIE-FrontEnd/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import './App.css';
-import HIEComponent from './components/HIEComponent';
-
+import React from "react";
+import "./App.css";
+import HIEComponent from "./components/HIEComponent";
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container-fluid">
-      <h4 className="navbar-brand">HIE there! </h4>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+      <div className="sticky-lg-top">
+        <div className="p-3 mb-2 bg-success text-white">
+          <h1 className="display-4">HIE there! </h1>
+          <p className="display-6">
+            This application provides you with all the necessary life saving
+            emergency health Services.
+          </p>
+        </div>
       </div>
-    </div>
-    </nav>
-    <div >
-      <HIEComponent />
-    </div>
+      <div>
+        <HIEComponent />
+      </div>
     </>
   );
 }
